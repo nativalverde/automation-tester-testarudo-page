be2d35dba3bc721cf29cb63b0a63301c3ebba91f
nativalverde <nnvalverdem@gmail.com>
2026-05-02 18:34:01 -0500

    env

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,24 +17,24 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  //forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://staging.testertestarudo.com/es',
-	video: "off",
-	screenshot: "only-on-failure",
-	viewport: { width: 1200, height: 720},
+	  video: "off",
+	  screenshot: "only-on-failure",
+	  viewport: { width: 1200, height: 720},
     trace: 'on-first-retry',
-	navigationTimeout: 30000,
+	  navigationTimeout: 30000,
   },
 
   /* Configure projects for major browsers */
